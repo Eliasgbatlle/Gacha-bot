0d6e5ec321677ffb6d3d73c038e20178bd5d78e6
Eliasgbatlle <72628721+Eliasgbatlle@users.noreply.github.com>
2025-04-17 04:06:05 +0000

    all

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import discord
 import os
 import json
 from dotenv import load_dotenv
+from utils.databasechar import crear_db, generar_personajes_objetivo
+
+crear_db()
+generar_personajes_objetivo(1000)
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
