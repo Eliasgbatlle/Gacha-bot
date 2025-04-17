79eb57320d3a1164974c30240d88f15d9618a31e
Eliasgbatlle <72628721+Eliasgbatlle@users.noreply.github.com>
2025-04-17 03:49:22 +0000

    6

--- utils/databasechar.py
+++ utils/databasechar.py
@@ -146,24 +146,25 @@ def generar_personajes_objetivo(objetivo=1000):
 
     print(f"✅ Se generaron {generados} personajes nuevos.")
 
-    def get_available_characters():
-        conn = sqlite3.connect("data/characters.db")
-        c = conn.cursor()
-        c.execute("SELECT nombre, rareza, serie, precio, precio_base FROM personajes WHERE estado = 'disponible'")
-        rows = c.fetchall()
-        conn.close()
+def get_available_characters():
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+    c.execute("SELECT nombre, rareza, serie, precio_actual, precio_base FROM personajes WHERE estado = 'disponible'")
+    rows = c.fetchall()
+    conn.close()
 
-        personajes = []
-        for row in rows:
-            personajes.append({
-                "nombre": row[0],
-                "rareza": row[1],
-                "serie": row[2],
-                "precio": row[3],
-                "precio_base": row[4]
-            })
-        
-        return personajes
+    personajes = []
+    for row in rows:
+        personajes.append({
+            "nombre": row[0],
+            "rareza": row[1],
+            "serie": row[2],
+            "precio": row[3],
+            "precio_base": row[4]
+        })
+    
+    return personajes
+
 
 
 if __name__ == "__main__":
