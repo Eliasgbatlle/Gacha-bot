5dd28e9b68d74a82454b7282272d207f7d082697
eliasgbatlle <eliasbatlle@gmail.com>
2025-04-23 14:07:13 -0500

    web

--- web/src/app/api/gacha/route.ts
+++ web/src/app/api/gacha/route.ts
@@ -1,25 +1,17 @@
 import { NextResponse } from 'next/server';
 import path from 'path';
-import sqlite3 from 'sqlite3';
+import Database from 'better-sqlite3';
 
 // Ruta a la base de datos
 const dbPath = path.join(process.cwd(), '../../gacha_data.db');
 
 export async function GET() {
-    return new Promise((resolve, reject) => {
-        const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err: Error | null) => {
-            if (err) {
-                reject(NextResponse.json({ error: 'Error al conectar con la base de datos' }, { status: 500 }));
-            }
-        });
-
-        db.all('SELECT * FROM gacha_table', (err: Error | null, rows: any[]) => {
-            if (err) {
-                reject(NextResponse.json({ error: 'Error al consultar la base de datos' }, { status: 500 }));
-            } else {
-                resolve(NextResponse.json(rows));
-            }
-            db.close();
-        });
-    });
+    try {
+        const db = new Database(dbPath, { readonly: true });
+        const rows = db.prepare('SELECT * FROM gacha_table').all();
+        db.close();
+        return NextResponse.json(rows);
+    } catch (error) {
+        return NextResponse.json({ error: 'Error al consultar la base de datos', details: error.message }, { status: 500 });
+    }
 }
