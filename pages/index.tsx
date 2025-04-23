5b086b62b0478e616fa3e4ff2ae85e6157cb32f9
eliasgbatlle <eliasbatlle@gmail.com>
2025-04-23 12:41:00 -0500

    xd

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+
+  return (
+    <main>
+      {session ? (
+        <>
+          <p>Bienvenido, {session.user?.name}</p>
+          <button onClick={() => signOut()}>Cerrar sesión</button>
+        </>
+      ) : (
+        <button onClick={() => signIn("discord")}>Iniciar sesión con Discord</button>
+      )}
+    </main>
+  );
+}
