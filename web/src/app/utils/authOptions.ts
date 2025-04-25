d7b590d20e4c0cddfebdc4bf9bf7ab2beafb3d1d
eliasgbatlle <eliasbatlle@gmail.com>
2025-04-24 20:53:19 -0500

    authOptions.ts

---
+++ web/src/app/utils/authOptions.ts
@@ -0,0 +1,11 @@
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
